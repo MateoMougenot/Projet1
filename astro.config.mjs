aa3d2b52009094945e88529c033f7e5753d3e7c7
MateoMougenot <mateo.mougenot@etu.unilim.fr>
2025-09-03 09:57:54 +0200

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MateoMougenot.github.io',
+  base: '/Projet1',
   vite: {
     plugins: [tailwindcss()]
   }
